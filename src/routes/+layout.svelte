73bfcae25171403f30706b3be977be0e88435445
Ayush Porwal <87272409+porwal-ayush@users.noreply.github.com>
2022-10-31 12:01:16 +0530

    built home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
-    import "../app.css";
-  </script>
-  
-  <slot />
+	import '../app.css';
+	import Navbar from '../components/navbar/Navbar.svelte';
+	import Footer from '../components/footer/Footer.svelte';
+</script>
+
+<Navbar />
+<slot />
+<Footer email="ping.ayushporwal@gmail.com" />
