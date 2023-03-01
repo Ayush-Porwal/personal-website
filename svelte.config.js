2f1c3c86b76f165f6090e000dff981d934104995
Ayush Porwal <aporwal@concertai.com>
2023-03-01 15:25:39 +0530

    reverting back changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,10 +10,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			default: true
-		}
+		adapter: adapter()
 	}
 };
 
