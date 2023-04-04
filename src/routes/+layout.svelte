3d76ced1eb1d7974471161f590e8ef8d62f23f30
Ayush Porwal <aporwal@concertai.com>
2023-04-04 15:07:40 +0530

    adding some info when user clicks the form submit button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+
 	import '../app.css';
 	import Navbar from '../components/navbar/Navbar.svelte';
 	import Footer from '../components/footer/Footer.svelte';
 </script>
 
+<SvelteToast />
 <Navbar />
 <slot />
 <Footer email="ping.ayushporwal@gmail.com" />
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
 	import Button from '../../components/button/Button.svelte';
+	import { toast } from '@zerodevx/svelte-toast';
+
+	const defaultValues = {
+		fullName: '',
+		email: '',
+		subject: '',
+		message: ''
+	};
+
+	let formData = { ...defaultValues };
 </script>
 
 <main class="flex flex-col justify-center min-h-[calc(100vh_-_124px)] overflow-auto w-3/4 mx-auto">
@@ -8,28 +18,59 @@
 		I’m interested to work in ambitious and large projects. However, if you have other request or
 		question, please don’t hesitate to contact.
 	</span>
-	<form class="flex flex-col gap-2">
+	<form
+		class="flex flex-col gap-2"
+		id="contact_form"
+		on:submit|preventDefault={() => {
+			formData = { ...defaultValues };
+			toast.push('Havent implemented the backend functionality yet...', {
+				duration: 5000,
+				theme: {
+					'--toastColor': '#fff',
+					'--toastBackground': 'rgba(100,255,218,0.1)',
+					'--toastBarBackground': 'rgba(100,255,218,0.1)'
+				}
+			});
+		}}
+	>
 		<fieldset class="flex flex-col md:flex-row w-full gap-2">
 			<input
 				type="text"
-				placeholder="Name"
+				placeholder="Full Name"
+				bind:value={formData.fullName}
+				required={true}
 				class="p-4 bg-[rgba(100,255,218,0.1)] w-full md:w-1/2 rounded text-white"
 			/>
 			<input
 				type="email"
 				placeholder="Email"
+				bind:value={formData.email}
+				required={true}
 				class="p-4 bg-[rgba(100,255,218,0.1)] w-full md:w-1/2 rounded text-white"
 			/>
 		</fieldset>
 		<input
 			type="text"
 			placeholder="Subject"
+			bind:value={formData.subject}
+			required={true}
 			class="p-4 bg-[rgba(100,255,218,0.1)] w-full rounded text-white"
 		/>
 		<textarea
 			placeholder="Message"
+			bind:value={formData.message}
+			required={true}
 			class="p-4 bg-[rgba(100,255,218,0.1)] w-full rounded text-white"
 		/>
-		<div class="flex justify-end w-full"><Button buttonTitle="Send Message!" /></div>
+		<div class="flex justify-end w-full">
+			<Button buttonTitle="Send Message!" />
+		</div>
 	</form>
 </main>
+
+<style>
+	:root {
+		--toastContainerTop: 80px;
+		--toastContainerRight: 16px;
+	}
+</style>
